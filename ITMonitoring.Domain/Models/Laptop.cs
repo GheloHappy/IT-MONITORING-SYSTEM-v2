c9792e352544afd07390fd3c5281d6e206792797
unknown <ghelonicomaligaya111@gmail.com>
2022-08-08 13:29:56 +0800

    init

---
+++ ITMonitoring.Domain/Models/Laptop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMonitoring.Domain.Models
+{
+    public class Laptop : DomainObject
+    {
+        public string Series { get; set; }
+        public string Transaction { get; set; }
+        public string Agreement { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Department { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public DateTime DateRecieved { get; set; }
+        public string Model { get; set; }
+        public string Memory { get; set; }
+        public string Storage { get; set; }
+        public string Remarks { get; set; }
+        public double Price { get; set; }
+        public string Serial { get; set; }
+        public string Charger { get; set; }
+        public string Others { get; set; }
+    }
+}
